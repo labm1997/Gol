6c274de72889c647509d424678c1260c81e9911b
labm1997 <labm1997@gmail.com>
2017-09-25 11:49:11 -0300

    Melhorias no build.sh, agora tem um verbose legal

--- build.sh
+++ build.sh
@@ -1,4 +1,42 @@
+normal="\x1B[0m"
+vermelho="\x1B[31m"
+amarelo="\x1B[33m"
+verde="\x1B[32m"
 
+if [ $1 == "limpar" ]; then
+	echo -e "[ "$verde"EXEC"$normal" ] Limpando arquivos de compilação"
+	if [ -d "bin" ]; then
+		rm bin -rf
+	else
+		echo -e "[ "$amarelo"NOTF"$normal" ] Não há bin/,  pulando"
+	fi
+	if [ -f "MANIFEST.MF" ]; then
+		rm MANIFEST.MF
+	fi
+	if [ -f "Gol.jar" ]; then
+		rm Gol.jar
+	fi
+	exit
+fi
+
+if [ ! -f "scala-library.jar" ]; then
+	if [ $# == 0 ]; then
+		echo -e "[ "$vermelho"ERRO"$normal" ] NÃO VAI DAR NÃO"
+		echo -e "[ "$amarelo"DICA"$normal" ] Preciso de um arquivo para funcionar, normalmente esse arquivo está em /usr/share/java/scala-library.jar"
+		echo -e "[ "$amarelo"DICA"$normal" ] Me diga onde está \"scala-library.jar\" passando como argumento:"
+		echo -e "[ "$amarelo"DICA"$normal" ] $ ./build.sh CAMINHO"
+		exit
+	elif [ ! -f $1 ]; then
+		echo -e "[ "$vermelho"ERRO"$normal" ] NÃO VAI DAR NÃO"
+		echo -e "[ "$vermelho"ERRO"$normal" ] NÃO ACHEI "$1
+		exit
+	else
+		if [ -f "MANIFEST.MF" ]; then
+			cp MANIFEST.MF MANIFEST.MF.old
+		fi
+		echo -e "Main-Class: br.unb.cic.poo.gol.Main\nClass-Path: "$1 > MANIFEST.MF
+	fi
+fi
 if [ ! -d "bin" ]; then
 	mkdir bin
 fi
