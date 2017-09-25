4532db79136005ba676f37c194353b52343c0032
labm1997 <labm1997@gmail.com>
2017-09-25 12:02:41 -0300

    modified:   build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
+# Parâmetros
+main="br.unb.cic.poo.gol.Main"
+
+# Cores
 normal="\x1B[0m"
 vermelho="\x1B[31m"
 amarelo="\x1B[33m"
 verde="\x1B[32m"
 
-if [ $1 == "limpar" ]; then
+# Faz a limpeza
+if [ "$1" == "limpar" ]; then
 	echo -e "[ "$verde"EXEC"$normal" ] Limpando arquivos de compilação"
 	if [ -d "bin" ]; then
 		rm bin -rf
@@ -19,24 +24,29 @@ if [ $1 == "limpar" ]; then
 	exit
 fi
 
-if [ ! -f "scala-library.jar" ]; then
-	if [ $# == 0 ]; then
-		echo -e "[ "$vermelho"ERRO"$normal" ] NÃO VAI DAR NÃO"
-		echo -e "[ "$amarelo"DICA"$normal" ] Preciso de um arquivo para funcionar, normalmente esse arquivo está em /usr/share/java/scala-library.jar"
-		echo -e "[ "$amarelo"DICA"$normal" ] Me diga onde está \"scala-library.jar\" passando como argumento:"
-		echo -e "[ "$amarelo"DICA"$normal" ] $ ./build.sh CAMINHO"
-		exit
-	elif [ ! -f $1 ]; then
-		echo -e "[ "$vermelho"ERRO"$normal" ] NÃO VAI DAR NÃO"
-		echo -e "[ "$vermelho"ERRO"$normal" ] NÃO ACHEI "$1
-		exit
-	else
-		if [ -f "MANIFEST.MF" ]; then
-			cp MANIFEST.MF MANIFEST.MF.old
-		fi
-		echo -e "Main-Class: br.unb.cic.poo.gol.Main\nClass-Path: "$1 > MANIFEST.MF
+# Erro por não passar o arquivo
+if [ $# == 0 ]; then
+	echo -e "[ "$vermelho"ERRO"$normal" ] NÃO VAI DAR NÃO"
+	echo -e "[ "$amarelo"DICA"$normal" ] Preciso de um arquivo para funcionar, normalmente esse arquivo está em /usr/share/java/scala-library.jar"
+	echo -e "[ "$amarelo"DICA"$normal" ] Me diga onde está \"scala-library.jar\" passando como argumento:"
+	echo -e "[ "$amarelo"DICA"$normal" ] $ ./build.sh CAMINHO"
+	exit
+
+# Erro por não achar o arquivo
+elif [ ! -f $1 ]; then
+	echo -e "[ "$vermelho"ERRO"$normal" ] NÃO VAI DAR NÃO"
+	echo -e "[ "$vermelho"ERRO"$normal" ] NÃO ACHEI "$1
+	exit
+
+# Gera o MANIFEST
+else
+	if [ -f "MANIFEST.MF" ]; then
+		cp MANIFEST.MF MANIFEST.MF.old
 	fi
+	echo -e "Main-Class: "$main"\nClass-Path: "$1 > MANIFEST.MF
 fi
+
+# Gera a pasta de compilação, compila e gera o Java Archive
 if [ ! -d "bin" ]; then
 	mkdir bin
 fi
