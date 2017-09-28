27516081e786e9eb99904beca07c8274fdf1a088
Luiz <luiz@LexServer.oluiz.com.br>
2017-09-28 15:48:45 -0300

    * Leitura do arquivo regras.txt, implementação da função encontrarRegras usando expressão lambda

--- app/src/main/scala/br/unb/cic/poo/gol/app/Main.scala
+++ app/src/main/scala/br/unb/cic/poo/gol/app/Main.scala
@@ -3,6 +3,10 @@ package br.unb.cic.poo.gol.app
 import br.unb.cic.poo.gol.base.GameEngine
 import br.unb.cic.poo.gol.base.GameController
 import br.unb.cic.poo.gol.base.Conway
+import br.unb.cic.poo.gol.base.Regra
+
+import scala.io.Source
+import scala.collection.mutable.MutableList
 
 /**
  * Programa principal do GoL.
@@ -16,4 +20,12 @@ object Main {
   def main(args: Array[String]){
     GameController.start
   }
+
+/*
+ * Retorna uma lista de regras de um arquivo texto
+ */
+  private def encontrarRegras(arquivo: String = "regras.txt") = 
+    Source.fromResource(arquivo).getLines().map( (a:String) => Class.forName(a).newInstance.asInstanceOf[Regra])
+
 }
+
